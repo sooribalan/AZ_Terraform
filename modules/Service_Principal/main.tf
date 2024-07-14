7df9ee972b5022172807572656da6bccc43dae45
soori <sooribalan@outlok.com>
2024-07-14 13:28:17 +0530

    fixed errors

--- modules/Service_Principal/main.tf
+++ modules/Service_Principal/main.tf
@@ -1,16 +1,16 @@
 data "azuread_client_config" "current" {}
 
-resource "azuread_application" "example" {
+resource "azuread_application" "spnapp" {
   display_name = var.service_principal_name
   owners       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_service_principal" "spn" {
-  client_id                    = azuread_application.example.client_id
+  client_id                    = azuread_application.spnapp.client_id
   app_role_assignment_required = true
   owners                       = [data.azuread_client_config.current.object_id]
 }
 
-resource "azuread_service_principal_password" "example" {
-  service_principal_id = azuread_service_principal.example.object_id
+resource "azuread_service_principal_password" "spnpass" {
+  service_principal_id = azuread_service_principal.spn.object_id
 }
