d2441add6de830d0fd92f936b5cfae379b75ab3c
soori <sooribalan@outlok.com>
2024-07-14 12:08:54 +0530

    changed storage account

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
    backend "azurerm" {
     resource_group_name  = "jenkins"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "aksstorage7305"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "tfstate7305"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    sas_token            = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-07-13T18:42:07Z&st=2024-07-13T10:42:07Z&spr=https&sig=zwCVvT%2FwSMuVwiVkaT%2F2DKzlY88m0JGOW2fBp7FbjC4%3D"  # Can also be set via `ARM_TENANT_ID` environment variable.
+    sas_token            = "sv=2022-11-02&ss=bfqt&srt=c&sp=rwdlacupiytfx&se=2024-07-14T14:36:44Z&st=2024-07-14T06:36:44Z&spr=https&sig=AWTWXONziNn9aXyaWWIFGMyj3lSbonfc2QZZJMXvSdg%3D"  # Can also be set via `ARM_TENANT_ID` environment variable.
 
   }
 
